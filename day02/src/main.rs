bb473fa3029919dc3bd4e5fe6ac280f73deae9c9
Lucas Bonnet <lbonnet@bearstech.com>
2022-12-08 19:26:25 +0100

    day2...

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,3 +1,123 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+#[derive(Debug, Copy, Clone)]
+enum Ply {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Result {
+    Win,
+    Draw,
+    Loss,
+}
+
+impl Ply {
+    fn shape_score(self) -> i32 {
+        match self {
+            Ply::Rock => 1,
+            Ply::Paper => 2,
+            Ply::Scissors => 3,
+        }
+    }
+
+    fn wins_against(self) -> Ply {
+        match self {
+            Ply::Rock => Ply::Scissors,
+            Ply::Paper => Ply::Rock,
+            Ply::Scissors => Ply::Paper,
+        }
+    }
+
+    fn draws_against(self) -> Ply {
+        self
+    }
+
+    fn loses_against(self) -> Ply {
+        match self {
+            Ply::Rock => Ply::Paper,
+            Ply::Paper => Ply::Scissors,
+            Ply::Scissors => Ply::Rock,
+        }
+    }
+    fn beats(self, other: &Ply) -> Result {
+        match (self, other) {
+            (Ply::Rock, Ply::Paper) => Result::Loss,
+            (Ply::Rock, Ply::Rock) => Result::Draw,
+            (Ply::Rock, Ply::Scissors) => Result::Win,
+
+            (Ply::Paper, Ply::Paper) => Result::Draw,
+            (Ply::Paper, Ply::Rock) => Result::Win,
+            (Ply::Paper, Ply::Scissors) => Result::Loss,
+
+            (Ply::Scissors, Ply::Paper) => Result::Win,
+            (Ply::Scissors, Ply::Rock) => Result::Loss,
+            (Ply::Scissors, Ply::Scissors) => Result::Draw,
+        }
+    }
+
+    fn score_against(self, other: &Ply) -> i32 {
+        match self.beats(other) {
+            Result::Win => self.shape_score() + 6,
+            Result::Draw => self.shape_score() + 3,
+            Result::Loss => self.shape_score(),
+        }
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let mut file = fs::read_to_string("./input").unwrap();
+    if file.ends_with('\n') {
+        file.pop();
+    }
+    let strategy: Vec<&str> = file.split('\n').collect();
+
+    let total_score: i32 = strategy.iter().fold(0, |total, round| {
+        let (left, right) = (round.chars().next().unwrap(), round.chars().nth(2).unwrap());
+        let attack = match left {
+            'A' => Ply::Rock,
+            'B' => Ply::Paper,
+            'C' => Ply::Scissors,
+            // should never land here with AoC controlled inputs
+            _ => panic!(),
+        };
+        let response = match right {
+            'X' => Ply::Rock,
+            'Y' => Ply::Paper,
+            'Z' => Ply::Scissors,
+            // should never land here with AoC controlled inputs
+            _ => panic!(),
+        };
+
+        total + response.score_against(&attack)
+    });
+
+    println!("x - {}", total_score);
+
+    // Part 2
+    let total_score: i32 = strategy.iter().fold(0, |total, round| {
+        let (left, right) = (round.chars().next().unwrap(), round.chars().nth(2).unwrap());
+        let attack = match left {
+            'A' => Ply::Rock,
+            'B' => Ply::Paper,
+            'C' => Ply::Scissors,
+            // should never land here with AoC controlled inputs
+            _ => panic!(),
+        };
+        // response is actually the desired outcome (X - lose, Y - draw, Z - win)
+        let response = match right {
+            'X' => attack.wins_against(),
+            'Y' => attack.draws_against(),
+            'Z' => attack.loses_against(),
+            // should never land here with AoC controlled inputs
+            _ => panic!(),
+        };
+
+        total + response.score_against(&attack)
+    });
+
+    println!("y - {}", total_score);
+    Ok(())
 }
